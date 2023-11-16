e344a79ba74190552e99562b86f56da5e3e153f9
A SAI SANKAR <116367689+ASAISANKAR@users.noreply.github.com>
2023-11-16 14:50:28 +0530

    Update Navbar.js

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -11,7 +11,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import AdbIcon from '@mui/icons-material/MilitaryTech';
 
 const pages = ['Home', 'Details', 'Projects','Events'];
 const settings = ['LinkedIn'];
